35b0f4d94d344424f6452fbd174900af6257b621
Amiel Monasterial <amielmon@gmail.com>
2021-07-19 21:30:15 -0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import {
+  extend,
+  render,
+  useExtensionInput,
+  BlockStack,
+  Button,
+  Heading,
+  Image,
+} from '@shopify/post-purchase-ui-extensions-react';
+
+extend('Checkout::PostPurchase::ShouldRender', async () => {
+  return { render : true };
+});
+
+render('Checkout::PostPurchase::Render', () => <App />);
+
+export function App() {
+  const {done} = useExtensionInput();
+  return (
+    <BlockStack spacing="loose" alignment="center">
+      <Heading>My first post-purchase extension</Heading>
+      <Button submit onPress={done}>
+        Click me
+      </Button>
+    </BlockStack>
+  )
+}
